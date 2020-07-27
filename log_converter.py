94f24b3b67c29fef2dbbc5aca4e0bcd4dc2df066
Gooogr <gusarovga@ya.ru>
2020-07-27 16:06:00 +0300

    Update log_converter.py

--- log_converter.py
+++ log_converter.py
@@ -1,22 +1,31 @@
 import os
 import re
+from collections import defaultdict
+import json  
 
 FILE_PATH = './log_naive.txt'
 
 file_lines = open(FILE_PATH, 'r').read()
+table_dict = defaultdict()
+current_jpg_name = ''
 
-# Get jpg files names
-delimiters = " ", "/", ":"
-regexPattern = '|'.join(map(re.escape, delimiters))
-
-jpg_names = []
+jpg_delimiters = " ", "/", ":"
+jpg_regexPattern = '|'.join(map(re.escape, jpg_delimiters))
 	
-for item in re.split(regexPattern, file_lines):
-	if '.jpg' in item:
-		jpg_names.append(item)
-		
-print(jpg_names)
-		
+for line in file_lines.splitlines():	
+	if '.jpg' in line:
+		for item in re.split(jpg_regexPattern, line):
+			if '.jpg' in item:
+				current_jpg_name = item
+				table_dict[item] = []
+	if '%' in line:
+		split_string = (re.findall('-?\d+', line))
+		split_string = list(filter(lambda x: x != "", split_string)) # remove empty strings from list
+		int_string = list(map(int, split_string))
+		sub_dict_keys = ['proba_%', 'left_x', 'top_y', 'width', 'height']
+		table_dict[current_jpg_name].append(dict(zip(sub_dict_keys, int_string)))
 
+with open("log.json", "w") as outfile:  
+    json.dump(table_dict, outfile) 
 
 
